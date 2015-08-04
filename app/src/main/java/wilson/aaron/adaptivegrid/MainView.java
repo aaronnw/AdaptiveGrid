810dd2c5cb010e54aadd332235baa703b9a36c5a
Aaron Wilson <aaronnw11@gmail.com>
2015-08-04 11:50:46 -0500

    Added Menu and strings

--- app/src/main/java/wilson/aaron/adaptivegrid/MainView.java
+++ app/src/main/java/wilson/aaron/adaptivegrid/MainView.java
@@ -1,7 +1,9 @@
 package wilson.aaron.adaptivegrid;
 
+import android.app.Activity;
+
 /**
  * Created by Aaron.Wilson on 8/4/2015.
  */
-public class MainView {
+public class MainView extends Activity{
 }
