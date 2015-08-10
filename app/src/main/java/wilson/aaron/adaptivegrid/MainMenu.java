36567ebe8cea0aee02382b8a72bad6176f3ba070
Aaron Wilson <aaronnw11@gmail.com>
2015-08-10 16:43:27 -0500

    Fixed IDs and added listeners

--- app/src/main/java/wilson/aaron/adaptivegrid/MainMenu.java
+++ app/src/main/java/wilson/aaron/adaptivegrid/MainMenu.java
@@ -4,27 +4,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TextView;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements View.OnClickListener{
+    Button testButton1;
+    Button testButton2;
+    Button testButton3;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         GridLayout myGrid = new GridLayout(this);
-        Button testButton1 = new Button(this);
+
+        testButton1 = new Button(this);
         testButton1.setText("Button 1");
+        testButton1.setId(R.id.button1ID);
+        testButton1.setOnClickListener(this);
 
-        Button testButton2 = new Button(this);
+        testButton2 = new Button(this);
         testButton2.setText("Button 2");
+        testButton2.setId(R.id.button2ID);
+        testButton2.setOnClickListener(this);
 
-        Button testButton3 = new Button(this);
+        testButton3 = new Button(this);
         testButton3.setText("Button 3");
+        testButton3.setId(R.id.button3ID);
+        testButton3.setOnClickListener(this);
+
+        result = new TextView(this);
+        result.setText("Nothing pressed");
 
         myGrid.addView(testButton1);
         myGrid.addView(testButton2);
         myGrid.addView(testButton3);
+        myGrid.addView(result);
+
         setContentView(myGrid);
     }
 
@@ -48,5 +67,16 @@ public class MainMenu extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == testButton1.getId()){
+            result.setText("Pressed 1!");
+        }else if(v.getId() == testButton2.getId()){
+           result.setText("Pressed 2!");
+        }else if(v.getId() == testButton3.getId() ){
+           result.setText("Pressed 3!");
+        }
     }
 }
